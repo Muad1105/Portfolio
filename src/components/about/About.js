4b139b88df937f9f94f8b1045193416cb2543e3a
Muad1105 <muadnoushad@gmail.com>
2023-08-10 07:31:12 +0530

    About update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,10 +7,10 @@ const About = () => {
       <h1>ABOUT</h1>
       <div>
         With an in-depth understanding of front-end web development, worked with
-        5 projects in organisational exposure specializing in creating dynamic
-        and interactive user interfaces. With a strong foundation in JavaScript,
-        HTML, and CSS, you excel in building robust and efficient React
-        applications I create fast, clean, and maintainable web applications.
+        projects specializing in creating dynamic and interactive user
+        interfaces. With a strong foundation in JavaScript, HTML, and CSS, I
+        build robust and efficient React applications and create fast, clean,
+        maintainable web applications.
       </div>
       <div>
         Expertise extends to integrating APIs, handling asynchronous operations,
@@ -20,8 +20,8 @@ const About = () => {
         delivering visually appealing and user-friendly interfaces.
       </div>
       <div>
-        With strong problem-solving skills have worked on 4 web applications and
-        contributing to the success of your development team.
+        With strong problem-solving skills I can contribute to the success of
+        your development team.
       </div>
     </div>
   );
