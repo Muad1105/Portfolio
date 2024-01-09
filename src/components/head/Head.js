f6422ecf1018474100aeb8fd5b91f34c86865717
muad1105 <muadnoushad@gmail.com>
2024-01-09 18:33:53 +0530

    header update

--- src/components/head/Head.js
+++ src/components/head/Head.js
@@ -6,8 +6,8 @@ const Head = () => {
     <div className="head-section">
       <p>Hi, My name is Muad Noushad</p>
       <h1 className="description">
-        I'm a Frontend Developer, Specialised in building Web Applications using
-        ReactJS, JavaScript, Typescript & More
+        I'm a MERN Stack Developer, Specialised in building Websites and Web
+        Applications using ReactJS, JavaScript, Typescript, Node and MongoDB.
       </h1>
     </div>
   );
